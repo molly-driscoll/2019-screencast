056fbd386657cfc3bc1be6da7fc8efae15c3c26c
billmeyer <thebillmeyer@gmail.com>
2019-05-01 08:13:39 -0400

    Start of demo

--- src/test/java/com/saucelabs/example/features/LoginFeatures.java
+++ src/test/java/com/saucelabs/example/features/LoginFeatures.java
@@ -2,48 +2,23 @@ package com.saucelabs.example.features;
 
 import com.saucelabs.example.pages.InventoryPage;
 import com.saucelabs.example.pages.LoginPage;
-import org.openqa.selenium.JavascriptExecutor;
-import org.openqa.selenium.remote.DesiredCapabilities;
-import org.openqa.selenium.remote.RemoteWebDriver;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
-import java.lang.reflect.Method;
 import java.net.MalformedURLException;
-import java.net.URL;
 
 public class LoginFeatures extends BaseFeature
 {
-    private static String username = System.getenv("SAUCE_USERNAME");
-    private static String accessKey = System.getenv("SAUCE_ACCESS_KEY");
-
-    @Test(dataProvider = "desktopDataProvider")
-    public void verifyValidUsersCanSignIn(String browser, String browserVersion, String platform, Method method)
+    @Test
+    public void verifyValidUsersCanSignIn()
     throws MalformedURLException
     {
-        URL url = new URL("https://ondemand.saucelabs.com:443/wd/hub");
-
-        DesiredCapabilities caps = new DesiredCapabilities();
-        caps.setCapability("browserName", browser);
-        caps.setCapability("platform", platform);
-        caps.setCapability("version", browserVersion);
-        caps.setCapability("avoidProxy", true);
-
-        caps.setCapability("username", username);
-        caps.setCapability("accessKey", accessKey);
-        caps.setCapability("name", "Verify Valid Users Can Sign In");
-        caps.setCapability("build", "build-1234");
-
-        caps.setCapability("extendedDebugging", true);
-        caps.setCapability("capturePerformance", true);
-
-        RemoteWebDriver driver = new RemoteWebDriver(url, caps);
+        WebDriver driver = new ChromeDriver();
 
         LoginPage loginPage = new LoginPage(driver);
         InventoryPage inventoryPage = new InventoryPage(driver);
-
-        JavascriptExecutor jsExec = (JavascriptExecutor)driver;
-        jsExec.executeScript("sauce:context=>>> Verify we are on the Inventory Page");
 
         loginPage.navigateTo(LoginPage.PAGE_URL);
         loginPage.enterUsername("standard_user");
@@ -55,7 +30,6 @@ public class LoginFeatures extends BaseFeature
         String currentUrl = driver.getCurrentUrl();
         Assert.assertEquals(currentUrl, InventoryPage.PAGE_URL, "Current URL does not match Expected");
 
-        jsExec.executeScript("sauce:job-result=true");
         driver.quit();
     }
 }
